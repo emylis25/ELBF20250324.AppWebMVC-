e3ca0427087617b6d81011abe081659101ce7133
Emily Lisbeth Bolaño Flores <2429841@clases.edu.sv>
2025-03-24 07:55:16 -0600

    Agregar archivos de proyecto.

---
+++ ELBF20250324.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ELBF20250324.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
